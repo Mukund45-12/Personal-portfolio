eb064cf4fed8849714fa8e478b4669461f8ed12a
Mukund Prasad Singh <111700599+Mukund45-12@users.noreply.github.com>
2024-02-21 00:03:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*--------------typing animation--------------*/
+var typed = new Typed(".typing", {
+    strings: ["Undergrad Student", "Software Engineer", "Web Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
